34b66605b7ba4bf8fb7afb89d30a75e40f819df4
Licheng-Guo <lichengguo777@gmail.com>
2021-01-11 17:04:13 -0800

    [FE] add rtl into the interface json

--- FE/CreateResultJson.py
+++ FE/CreateResultJson.py
@@ -8,12 +8,17 @@ class CreateResultJson:
     self.s2v = floorplan.getSlotToVertices()
     self.s2e = floorplan.getSlotToEdges()
     self.wrapper_creater = wrapper_creater
-  
+
   def createResultJson(self, file = 'FE_result.json'):
     result = {}
     result['FloorplanVertex'] = self.floorplan.getSlotNameToVertexNames()
     result['FloorplanEdge'] = self.floorplan.getSlotNameToEdgeNames()
     result['SlotIO'] = self.wrapper_creater.getSlotToIO()
+    
+    slot_to_rtl = {}
+    for slot in self.s2v.keys():
+      slot_to_rtl[slot.getRTLModuleName()] = self.wrapper_creater.createSlotWrapper(slot)
+    result['SlotWrapperRTL'] = slot_to_rtl
 
     f = open(file, 'w')
     f.write(json.dumps(result, indent=2))
