e5dfd2a056fe84dc23a142ad71cb13de9e54c8da
Blaok <i@blaok.me>
2021-01-21 20:34:55 -0800

    [FE] integrate with tapa

--- python/autoparallel/FE/tapa/ProgramJsonManager.py
+++ python/autoparallel/FE/tapa/ProgramJsonManager.py
@@ -1,96 +1,16 @@
-#! /usr/bin/python3.6
-
 from collections import defaultdict
-from pyverilog.vparser.parser import parse as rtl_parse
-import pyverilog.vparser.ast as ast
-from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
 import re
 import logging
-import json
 import os.path
 import collections, functools, operator
 
 class ProgramJsonManager:
-  def __init__(self, project_path, top_name):
+  def __init__(self, project_path, fifo_section, v_name_to_v_module):
     self.project_path = project_path
-    self.top_rtl_path = f'{project_path}/hdl/{top_name}_{top_name}.v'
-    program_json_path = f'{project_path}/program.json'
-    self.program = json.loads(open(program_json_path).read())
-    self.top_name = top_name
+    self.fifo_section = fifo_section
 
-    # be consistent with the RTL
-    self.__updateNaming()
-
-    # update json to include fifo width
-    self.e_name_to_width = {}
-    self.__extractFIFOWidth()
-
-    self.v_name_to_v_module = {}
+    self.v_name_to_v_module = v_name_to_v_module
     self.v_module_to_area = {}
-
-    self.__initVertexMapping()
-
-  def __DFS(self, node, filter_func):
-    if filter_func(node):
-      try:
-        logging.debug(f'visit node {node.name}')
-      except:
-        pass
-        # logging.debug(f'node in line {node.lineno} has no name')
-      yield node
-    for c in node.children():
-      yield from self.__DFS(c, filter_func)
-
-  def __traverseEdgeInAST(self):
-    top_module_ast, directives = rtl_parse([self.top_rtl_path]) 
-    is_edge_node = lambda node : isinstance(node, ast.Instance) and 'fifo' in node.module
-    yield from self.__DFS(top_module_ast, is_edge_node)
-
-  def __extractFIFOWidth(self):
-    for e_node in self.__traverseEdgeInAST():
-      name = e_node.name
-      for param in e_node.parameterlist:
-        if 'WIDTH' in param.paramname:
-          self.e_name_to_width[name] = int(param.argname.value)
-          break
-      assert name in self.e_name_to_width, f'{name}'
-
-    # update json
-    fifos_dict = self.getFIFOSection()
-
-    for e_name, info in fifos_dict.items():
-      info['width'] = self.e_name_to_width[e_name]
-
-  def __initVertexMapping(self):
-    fifos_dict = self.getFIFOSection()
-
-    for e_name, info in fifos_dict.items():
-      src = info['produced_by']
-      dst = info['consumed_by']
-      src_name = f'{src[0]}_{src[1]}'
-      src_module = src[0]
-      dst_name = f'{dst[0]}_{dst[1]}'
-      dst_module = dst[0]
-
-      self.v_name_to_v_module[src_name] = src_module
-      self.v_name_to_v_module[dst_name] = dst_module     
-
-  def __updateNaming(self):
-    fifos_dict = self.getFIFOSection()
-    fifos_dict_new = {}
-
-    # change from fifo_name[0] to fifo_name_0
-    for e_name_orig, info in fifos_dict.items():
-      if re.search(r'\[\d+\]', e_name_orig):
-        e_name = re.sub(r'\[(\d+)\]', r'_\1', e_name_orig)
-        fifos_dict_new[e_name] = info
-    
-    # change from [kernel_name, 0] to kernel_name_0
-    for e_name, info in fifos_dict_new.items():
-      info['produced_by_module_name'] = info['consumed_by'][0] + '_' + str(info['consumed_by'][1])
-      info['consumed_by_module_name'] = info['consumed_by'][0] + '_' + str(info['consumed_by'][1])
-
-    self.__updateFIFOSection(fifos_dict_new)    
 
   def __getAreaBasedOnIndividualReport(self, mod_type):
     rpt_addr = f'{self.project_path}/report/{mod_type}_csynth.rpt'
@@ -111,11 +31,8 @@ class ProgramJsonManager:
 
     assert False, 'Error in parsing the HLS report'
   
-  def __updateFIFOSection(self, fifo_sec_new):
-    self.program['tasks'][self.top_name]['fifos'] = fifo_sec_new
-
   def getFIFOSection(self):
-    return self.program['tasks'][self.top_name]['fifos']
+    return self.fifo_section
 
   def getVNameToModule(self):
     return self.v_name_to_v_module
