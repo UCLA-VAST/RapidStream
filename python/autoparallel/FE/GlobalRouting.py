1f4566fad6cb47e21bf51637d5f304c66e6271f8
Licheng-Guo <lichengguo777@gmail.com>
2021-02-24 15:57:59 -0800

    [FE] add naive method to determine latency for inter-slot edges

--- python/autoparallel/FE/GlobalRouting.py
+++ python/autoparallel/FE/GlobalRouting.py
@@ -2,6 +2,7 @@
 import logging
 import json
 from collections import defaultdict 
+from autoparallel.FE.DataflowGraph import Vertex, Edge
 
 class GlobalRouting:
   def __init__(self, floorplan, top_rtl_parser):
@@ -9,6 +10,7 @@ class GlobalRouting:
     self.top_rtl_parser = top_rtl_parser
     self.v2s = floorplan.getVertexToSlot()
     self.s2v = floorplan.getSlotToVertices()
+    self.e2lat = {}
 
   def naivePathPlanningFIFO(self):
     slot_to_dir = {}
@@ -78,6 +80,23 @@ class GlobalRouting:
 
   def naivePathPlanningWire(self):
     return self.getPathPlanningWire(self.naivePathPlanningFIFO())
+
+  def getPipelineLevelOfEdge(self, e : Edge) -> int:
+    src_slot = self.v2s[e.src]
+    dst_slot = self.v2s[e.dst]
+
+    src_x = src_slot.getPositionX()
+    src_y = src_slot.getPositionY()
+    dst_x = dst_slot.getPositionX()
+    dst_y = dst_slot.getPositionY()
+    dist = abs(src_x - dst_x) + abs(src_y - dst_y)
+
+    # add a register every 2 clock regions
+    # 1 unit of latency for original FIFO
+    lat = max( int(dist / 2), 1) 
+    logging.info(f'edge {e.name}: ({src_x}, {src_y}) -> ({dst_x}, {dst_y}); latency : {lat}')
+    
+    return lat
 
 if __name__ == '__main__':
   json_path = './BE_pass1_anchored.json'
