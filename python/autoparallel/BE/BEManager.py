650e123250b6a83711e5b36b7d12aad30bf0ca98
Licheng-Guo <lichengguo777@gmail.com>
2021-04-01 10:19:05 -0700

    [Stitch] create stitch scripts for all pairs. TODO: anchor placement extraction

--- python/autoparallel/BE/BEManager.py
+++ python/autoparallel/BE/BEManager.py
@@ -8,26 +8,50 @@ from autoparallel.BE.GenAnchorConstraints import *
 from autoparallel.BE.CreateVivadoRun import *
 from autoparallel.BE.AnchorPlacement import *
 from autoparallel.BE.DuplicateRTL import *
+from autoparallel.BE.CreatePairwiseWrapper import *
 
-if __name__ == '__main__':
-  assert len(sys.argv) == 3, 'input (1) the path to the front end result file and (2) the target directory'
-  backend_run_dir = sys.argv[2]
-  fe_result_path = sys.argv[1]
+def parallelAnchorPlacement(
+    hub, 
+    run_dir,
+    stitch_dir):
+  """
+  for each pair of neighbor slots, group them and place & router the anchors in between
+  TODO: extract the anchor placement 
+  """
+  pair_list = hub["ComputeSlotPairs"]
+  for pair in pair_list:
+    wrapper_name = '_AND_'.join(pair)
+    dir = f'{stitch_dir}/' + wrapper_name
+    os.mkdir(dir)
+    
+    # generate wrapper rtl
+    pair_wrapper = CreateWrapperForSlotPair(hub, pair[0], pair[1])
+    wrapper_path = f'{dir}/{wrapper_name}.v'
+    open(wrapper_path, 'w').write('\n'.join(pair_wrapper))
 
-  if os.path.isdir(backend_run_dir):
-    raise f'target directory already exists: {backend_run_dir}'
-  os.mkdir(backend_run_dir)
+    # generate clock constraint
+    createClockXDC(wrapper_name, dir)
+    clock_xdc_path = f'{dir}/{wrapper_name}_clk.xdc' # TODO: redundancy
 
-  hub = json.loads(open(fe_result_path, 'r').read())
+    # generate vivado script
+    dcp_path = lambda name : f'{run_dir}/{name}/{name}_routed_free_run/{name}_routed_free_run.dcp'
+    dcp_name2path = {name : dcp_path(name) for name in pair}
+    createVivadoScriptForSlotPair(hub, wrapper_name, wrapper_path, dcp_name2path, clock_xdc_path, dir)
+
+def parallelSlotRun(hub, run_dir):
+  """
+  generate scripts to place & route each slot independently
+  """
   fpga_part_name = hub['FPGA_PART_NAME']
   orig_rtl_path = hub['ORIG_RTL_PATH']
   FloorplanVertex = hub['FloorplanVertex']
 
   assert os.path.isdir(orig_rtl_path)
   
+  # TODO: take care of pure routing slots
   for slot_name in hub['SlotIO'].keys():
     logging.info(f'processing slot {slot_name}...')
-    dir = f'{backend_run_dir}/{slot_name}'
+    dir = f'{run_dir}/{slot_name}'
     os.mkdir(dir)
     
     # duplicate source RTL and add unique prefix
@@ -54,6 +78,23 @@ if __name__ == '__main__':
     # TODO: monitor when the placement of the free run is finished
     # createAnchorPlacementScript(hub, slot_name, backend_run_dir)
 
-  createGNUParallelScript(hub, backend_run_dir)
+  createGNUParallelScript(hub, run_dir)
 
-  
+if __name__ == '__main__':
+  assert len(sys.argv) == 3, 'input (1) the path to the front end result file and (2) the target directory'
+  backend_run_dir = sys.argv[2]
+  fe_result_path = sys.argv[1]
+
+  if os.path.isdir(backend_run_dir):
+    raise f'target directory already exists: {backend_run_dir}'
+  os.mkdir(backend_run_dir)
+
+  run_dir = f'{backend_run_dir}/parallel_run'
+  stitch_dir = f'{backend_run_dir}/parallel_stitch'
+  os.mkdir(run_dir)
+  os.mkdir(stitch_dir)
+
+  hub = json.loads(open(fe_result_path, 'r').read())
+
+  parallelSlotRun(hub, run_dir)
+  parallelAnchorPlacement(hub, run_dir, stitch_dir)
--- python/autoparallel/BE/CreatePairwiseWrapper.py
+++ python/autoparallel/BE/CreatePairwiseWrapper.py
@@ -19,12 +19,16 @@ def getIODecl(slot1_io : dict, slot2_io : dict, top_io : dict):
   # io_decl += ['  output ap_ready,']
   io_decl += ['  input ap_continue,']
   io_decl += ['  input ap_clk,']
-  io_decl += ['  input ap_rst_n']
+  io_decl += ['  input ap_rst_n);']
   return io_decl
 
 def getPipeline(inner_connection, pipeline_level):
   pipeline = []
-  for io, dir_width in inner_connection.items():  
+  for io, dir_width in inner_connection.items():
+    # filter control signals
+    if io.startswith('ap_'):
+      continue
+
     # assign the input wire equals the output wire
     if pipeline_level == 0:
       pipeline.append(f'  assign {io}_in = {io}_out;')
@@ -33,6 +37,8 @@ def getPipeline(inner_connection, pipeline_level):
       for i in range(pipeline_level):
         width = dir_width[1] if len(dir_width) == 2 else ''
         pipeline.append(f'  (* dont_touch = "yes" *) reg {width} {io}_q{i};')
+        pipeline.append(f'  wire {width} {io}_in;')
+        pipeline.append(f'  wire {width} {io}_out;')
     
       # connect the head and tail
       pipeline.append(f'  always @ (posedge ap_clk) begin')
@@ -72,7 +78,19 @@ def getInstance(slot_name : str, slot_io : dict, top_io : dict, inner_connection
 
   return instance
 
+def getEmptyWrapper(slot_name, slot_io):
+  """ in order for black_box modules to be recognized by vivado """
+  empty_wrapper = []
+  empty_wrapper.append(f'\n\nmodule {slot_name} (')
+  empty_wrapper += [' '.join(dir_width) + ' ' + io + ',' for io, dir_width in slot_io.items()]
+  empty_wrapper[-1] = empty_wrapper[-1].replace(',', '')
+  empty_wrapper.append(f');')
+  empty_wrapper.append(f'endmodule')
+  return empty_wrapper
+
 def CreateWrapperForSlotPair(hub, slot1_name, slot2_name, pipeline_level = 1):
+  """ group together two neighbor slots """
+  
   slot1_io = hub['SlotIO'][slot1_name]
   slot2_io = hub['SlotIO'][slot2_name]
 
@@ -93,20 +111,69 @@ def CreateWrapperForSlotPair(hub, slot1_name, slot2_name, pipeline_level = 1):
 
   # pipelined connection between the two slots
   pipeline = getPipeline(inner_connection, pipeline_level)
-  
+
   slot1_inst = getInstance(slot1_name, slot1_io, top_io, inner_connection)
   slot2_inst = getInstance(slot2_name, slot2_io, top_io, inner_connection)
   ending = ['endmodule\n']
 
-  return header + io_decl + pipeline + slot1_inst + slot2_inst + ending
+  slot1_def = getEmptyWrapper(slot1_name, slot1_io)
+  slot2_def = getEmptyWrapper(slot2_name, slot2_io)
 
-def CreateWrapperForAllSlotPairs(hub):
+  return header + io_decl + pipeline + slot1_inst + slot2_inst + ending + slot1_def + slot2_def
+
+def createWrapperForAllSlotPairs(hub):
   pairs = hub['ComputeSlotPairs']
   for pair in pairs:
     pair_wrapper = CreateWrapperForSlotPair(hub, pair[0], pair[1])
     open(f'{"_AND_".join(pair)}.v', 'w').write('\n'.join(pair_wrapper))
 
+def createVivadoScriptForSlotPair(
+    hub, 
+    wrapper_name,
+    wrapper_path, 
+    dcp_name2path : dict,
+    clk_xdc_path,
+    output_dir = '.'):
+  fpga_part_name = hub["FPGA_PART_NAME"]
+
+  script = []
+
+  script.append(f'set_part {fpga_part_name}')
+
+  # read in the original RTLs by HLS
+  script.append(f'read_verilog "{wrapper_path}"')  
+
+  # clock xdc
+  script.append(f'read_xdc "{clk_xdc_path}"')
+
+  # synth
+  script.append(f'synth_design -top "{wrapper_name}" -part {fpga_part_name} -mode out_of_context')
+  script.append(f'write_checkpoint synth.dcp')
+
+  # read in the dcp of slots
+  for name, path in dcp_name2path.items():
+    script.append(f'read_checkpoint -cell {name}_U0 {path}')
+
+  # constrain the pipeline registers. get the pblocks based on slot names
+  script.append(f'delete_pblock [get_pblock *]')
+  script.append(f'create_pblock wrapper')
+  script.append(f'set_property CONTAIN_ROUTING 1 [get_pblocks wrapper]')
+  script.append( 'add_cells_to_pblock [get_pblocks wrapper] [get_cells -regexp {.*_reg.*} ]')
+  for name, path in dcp_name2path.items():
+    # convert slot name to pblock
+    pblock = name.replace('CR', 'CLOCKREGION').replace('_To_', ':')
+    script.append(f'resize_pblock [get_pblocks wrapper] -add {pblock}')
+  
+  # place and anchors
+  script.append(f'place_design')
+
+  # extract anchor placement
+
+  script.append(f'write_checkpoint {wrapper_name}_placed.dcp')
+
+  open(f'{output_dir}/place.tcl', 'w').write('\n'.join(script))
+
 if __name__ == '__main__':
   hub_addr = sys.argv[1]
   hub = json.loads(open(hub_addr, 'r').read())
-  CreateWrapperForAllSlotPairs(hub)
+  createWrapperForAllSlotPairs(hub)
