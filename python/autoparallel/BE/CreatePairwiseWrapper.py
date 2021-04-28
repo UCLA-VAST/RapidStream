0096826764bf81ef846df368e8d8ce52e43d1f0a
Licheng-Guo <lichengguo777@gmail.com>
2021-04-27 18:33:47 -0700

    [BE] update pair wrapper creation for ctrl wrapper

--- python/autoparallel/BE/CreatePairwiseWrapper.py
+++ python/autoparallel/BE/CreatePairwiseWrapper.py
@@ -3,20 +3,21 @@ import logging
 import json
 import sys
 from autoparallel.BE.GenAnchorConstraints import createAnchorPlacementExtractScript
+from typing import List, Set, Dict, Tuple
 
 def getHeader(slot1_name, slot2_name):
   header = ['\n\n`timescale 1 ns / 1 ps',
             f'module {slot1_name}_AND_{slot2_name} (']
   return header
 
-def getIODecl(slot1_io : dict, slot2_io : dict, top_io : dict):
+def getIODecl(slot1_io : dict, slot2_io : dict, wrapper_io : dict):
   slot_io_merge = {**slot1_io, **slot2_io}
-  io_decl = [f'  {" ".join(slot_io_merge[io])} {io},' for io in top_io]
+  io_decl = [f'  {" ".join(slot_io_merge[io])} {io},' for io in wrapper_io]
   io_decl[-1] = io_decl[-1].replace(',', ');')
   
   return io_decl
 
-def getConnection(inner_connection, top_io, pipeline_level=1):
+def getConnection(inner_connection, pipeline_level=1):
   """
   get the RTL to connect the slots
   data links will be pipelined
@@ -60,11 +61,11 @@ def getConnection(inner_connection, top_io, pipeline_level=1):
 
   return connection, pipeline_regs
 
-def getInstance(slot_name : str, slot_io : dict, top_io : dict, inner_connection : dict):
+def getInstance(slot_name : str, slot_io : dict, wrapper_io : dict, inner_connection : dict):
   instance = []
   instance.append(f'  (* black_box *) {slot_name} {slot_name}_U0 (')
   for io, dir_width in slot_io.items():
-    if io in top_io:
+    if io in wrapper_io:
       instance.append(f'    .{io}({io}),')
     elif io in inner_connection:
       if 'input' in dir_width:
@@ -89,6 +90,28 @@ def getEmptyWrapper(slot_name, slot_io):
   empty_wrapper.append(f'endmodule')
   return empty_wrapper
 
+def getTopIOAndInnerConnectionOfPair(hub, slot1_name, slot2_name) -> Tuple[Set, Dict]:
+  slot1_io = hub['SlotIO'][slot1_name]
+  slot2_io = hub['SlotIO'][slot2_name]
+
+  # io name -> dir + width (in string)
+  convert = lambda slot_io : {io[-1] : io[0:-1] for io in slot_io }
+  slot1_io = convert(slot1_io)
+  slot2_io = convert(slot2_io)
+
+  # common io of the two slots
+  inner_connection = {k : v for k, v in slot1_io.items() if k in slot2_io}
+  
+  # external io of the wrapper
+  wrapper_io = slot1_io.keys() - slot2_io.keys() | \
+           slot2_io.keys() - slot1_io.keys()
+
+  # deal with ap_clk
+  wrapper_io.add('ap_clk')
+  inner_connection.pop('ap_clk')
+
+  return wrapper_io, inner_connection
+
 def CreateWrapperForSlotPair(hub, slot1_name, slot2_name, pipeline_level, output_dir, wrapper_name):
   """ 
   group together two neighbor slots 
@@ -103,25 +126,16 @@ def CreateWrapperForSlotPair(hub, slot1_name, slot2_name, pipeline_level, output
   slot1_io = convert(slot1_io)
   slot2_io = convert(slot2_io)
 
-  # common io of the two slots
-  inner_connection = {k : v for k, v in slot1_io.items() if k in slot2_io}
-  
-  # external io of the wrapper
-  top_io = slot1_io.keys() - slot2_io.keys() | \
-           slot2_io.keys() - slot1_io.keys()
-
-  # deal with ap_clk
-  top_io.add('ap_clk')
-  inner_connection.pop('ap_clk')
+  wrapper_io, inner_connection = getTopIOAndInnerConnectionOfPair(hub, slot1_name, slot2_name)
 
   header = getHeader(slot1_name, slot2_name)
-  io_decl = getIODecl(slot1_io, slot2_io, top_io)
+  io_decl = getIODecl(slot1_io, slot2_io, wrapper_io)
 
   # pipelined connection between the two slots
-  connection, pipeline_reg = getConnection(inner_connection, top_io, pipeline_level)
+  connection, pipeline_regs = getConnection(inner_connection, pipeline_level)
 
-  slot1_inst = getInstance(slot1_name, slot1_io, top_io, inner_connection)
-  slot2_inst = getInstance(slot2_name, slot2_io, top_io, inner_connection)
+  slot1_inst = getInstance(slot1_name, slot1_io, wrapper_io, inner_connection)
+  slot2_inst = getInstance(slot2_name, slot2_io, wrapper_io, inner_connection)
   ending = ['endmodule\n']
 
   slot1_def = getEmptyWrapper(slot1_name, slot1_io)
@@ -132,7 +146,7 @@ def CreateWrapperForSlotPair(hub, slot1_name, slot2_name, pipeline_level, output
   open(f'{output_dir}/{wrapper_name}.v', 'w').write('\n'.join(pair_wrapper))
 
   # in the meantime create the placement extraction script
-  createAnchorPlacementExtractScript(f'{slot1_name}_AND_{slot2_name}', pipeline_reg, output_dir)
+  createAnchorPlacementExtractScript(f'{slot1_name}_AND_{slot2_name}', pipeline_regs, output_dir)
 
   return pair_wrapper
 
