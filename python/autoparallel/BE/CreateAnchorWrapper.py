7a87e9badb1e0447c6b2cd711cd9281de6e18ad7
Licheng-Guo <lichengguo777@gmail.com>
2021-04-28 19:04:24 -0700

    [BE, refactor] organize code for anchor legalization

--- python/autoparallel/BE/CreateAnchorWrapper.py
+++ python/autoparallel/BE/CreateAnchorWrapper.py
@@ -2,18 +2,55 @@
 import logging
 import json
   
-def createAnchorWrapper(fe_result, slot_name, output_path='.'):
-  slot_to_io = fe_result['SlotIO']
-  slot_to_rtl = fe_result['SlotWrapperRTL']
+def getStrictAnchoredIO(hub, slot_name):
+  """ which IOs will connect to other slots with anchor registers """
+  anchored_io_and_wired_io = getAnchoredIOAndWiredIO(hub, slot_name)
+  return [io for io in anchored_io_and_wired_io if '_pass_0' in io[-1]]
+
+def getAnchoredIOAndWiredIO(hub, slot_name):
+  """ 
+  which IOs of the slot will connect to other slots
+  In the actual top RTL, some IOs will be registered while some are connected by direct wire
+  However, in this wrapper we also create an anchor register for those wire-connected IOs
+  because we need to properly constrain the orientation of the connection
+  """
+  slot_to_io = hub['SlotIO']
   io_list = slot_to_io[slot_name]
 
-  all_top_io = fe_result['TopIO']
+  all_top_io = hub['TopIO']
   top_io_names = set(io[-1] for io in all_top_io)
 
-  internal_io_list = [io for io in io_list if io[-1] not in top_io_names]
-  local_top_io_list = [io for io in io_list if io[-1] in top_io_names]
+  non_top_io_list = [io for io in io_list if io[-1] not in top_io_names] # not in top_io
+  return non_top_io_list
 
-  file = open(f'{output_path}/{slot_name}_anchored.v', 'w')
+def getTopLevelPorts(hub, slot_name):
+  """
+  get those IOs of the slot that belong to the top-level ports
+  They will be left unconstrainted 
+  """
+  slot_to_io = hub['SlotIO']
+  io_list = slot_to_io[slot_name]
+
+  all_top_io = hub['TopIO']
+  top_io_names = set(io[-1] for io in all_top_io)
+
+  local_top_io_list = [io for io in io_list if io[-1] in top_io_names] # in top_io
+  return local_top_io_list
+
+def createAnchorWrapper(hub, slot_name, output_path='.'):
+  """
+  Top-level ports will be directly connected
+  All other IOs will be registered
+  """
+  slot_to_io = hub['SlotIO']
+  slot_to_rtl = hub['SlotWrapperRTL']
+  io_list = slot_to_io[slot_name]
+
+  # IOs that are not top-level port
+  non_top_io_list = getAnchoredIOAndWiredIO(hub, slot_name)
+  
+  # top-level ports are unconstrained
+  local_top_io_list = getNonAnchoredIO(hub, slot_name)
 
   wrapper = []
   
@@ -25,18 +62,18 @@ def createAnchorWrapper(fe_result, slot_name, output_path='.'):
   wrapper.append('    ' + ' '.join(io_list[-1]) + ');')
 
   # anchor registers
-  for io in internal_io_list:
+  for io in non_top_io_list:
     wrapper.append('  ' + '(* dont_touch = "yes" *) reg ' + ' '.join(io[1:]) + '_q0' + ';')
 
   # wire connection to inst
-  for io in internal_io_list:
+  for io in non_top_io_list:
     if io[0] == 'output':
       wrapper.append('  ' + 'wire ' + ' '.join(io[1:]) + '_internal' + ';')
       wrapper.append(f'  assign {io[-1]} = {io[-1]}_q0;')
 
   # connect anchors
   wrapper.append('  ' + 'always @ (posedge ap_clk) begin')
-  for io in internal_io_list:
+  for io in non_top_io_list:
     if io[0] == 'input':
       wrapper.append('    ' + f'{io[-1]}_q0 <= {io[-1]};')
     elif io[0] == 'output':
@@ -48,7 +85,7 @@ def createAnchorWrapper(fe_result, slot_name, output_path='.'):
   # instantiate slot wrapper. DO NOT change the suffix '_U0'
   wrapper.append('  ' + f'(* dont_touch = "yes" *) {slot_name}_ctrl {slot_name}_U0 (' )
   for io in io_list: # try to preserve order
-    if io in internal_io_list:
+    if io in non_top_io_list:
       if io[0] == 'input':
         wrapper.append('    ' + f'.{io[-1]}({io[-1]}_q0),')
       elif io[0] == 'output':
@@ -60,6 +97,7 @@ def createAnchorWrapper(fe_result, slot_name, output_path='.'):
   # ending
   wrapper.append('endmodule')
 
+  file = open(f'{output_path}/{slot_name}_anchored.v', 'w')
   file.write('\n'.join(wrapper))
 
   # add the rtl for the inner module (the slot wrapper)
@@ -69,7 +107,7 @@ def createAnchorWrapper(fe_result, slot_name, output_path='.'):
   file.write('\n'.join(slot_to_rtl[slot_name][1:]))
 
 if __name__ == '__main__':
-  fe_result_path = '../FE/FE_result.json'
-  fe_result = json.loads(open(fe_result_path, 'r').read())
-  for slot_name in fe_result['SlotIO'].keys():
-    createAnchorWrapper(fe_result, slot_name)
+  fe_result_path = '../FE/hub.json'
+  hub = json.loads(open(fe_result_path, 'r').read())
+  for slot_name in hub['SlotIO'].keys():
+    createAnchorWrapper(hub, slot_name)
