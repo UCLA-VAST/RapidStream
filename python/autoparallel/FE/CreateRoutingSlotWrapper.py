41153ca08b53967dce8aaffdee6a7605d5372819
Licheng-Guo <lichengguo777@gmail.com>
2021-05-02 10:54:28 -0700

    [FE] fix bug in ad48171, the pipelined full_n is not correctly connected

--- python/autoparallel/FE/CreateRoutingSlotWrapper.py
+++ python/autoparallel/FE/CreateRoutingSlotWrapper.py
@@ -231,8 +231,9 @@ class CreateRoutingSlotWrapper:
           elif port_name.endswith('_full_n'):
             wire_width = self.top_rtl_parser.getWidthOfRegOrWire(wire_name)
             assert connection_stmt[-1] == f'wire {wire_width} {wire_name};'
-            connection_stmt[-1] = f'reg {wire_width} {wire_name}_q_for_broadcast;'
+            connection_stmt.append(f'reg {wire_width} {wire_name}_q_for_broadcast;')
             connection_stmt.append(f'always @ (posedge ap_clk) {wire_name}_q_for_broadcast <= {wire_name}_pass_0;')
+            connection_stmt.append(f'assign {wire_name} = {wire_name}_q_for_broadcast;')
 
     return connection_stmt
 
