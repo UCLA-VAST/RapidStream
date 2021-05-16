e32765e6f0e445c81f3eaeba4c5c2fa304037666
Licheng-Guo <lichengguo777@gmail.com>
2021-05-16 14:57:50 -0700

    [prune] fix bugs in script

--- tcl/removeAnchorRegs.tcl
+++ tcl/removeAnchorRegs.tcl
@@ -1,4 +1,9 @@
 set anchors [get_cells -regexp ".*_q0_reg.{0,6}"]
+set all_dead_nets []
+set all_chosen_nets []
+set all_target_ports []
+
+set num [llength $anchors]
 
 foreach anchor $anchors {
   set dead_net [ get_nets -of_objects [get_cells $anchor] -filter { NAME !~ "*ap_clk*" && NAME !~ "*const*" && ROUTE_STATUS == "HIERPORT" } ]
@@ -8,15 +13,26 @@ foreach anchor $anchors {
   set_property DONT_TOUCH 0 [get_nets $dead_net]
   set_property DONT_TOUCH 0 [get_nets $chosen_net]
 
+  lappend all_dead_nets $dead_net
+  lappend all_chosen_nets $chosen_net
+  lappend all_target_ports $target_port
+
+}
+
+for {set i 0} {$i < $num} {incr i} {
+  set anchor [lindex $anchors $i]
+  set dead_net [lindex $all_dead_nets $i]
+  set chosen_net [lindex $all_chosen_nets $i]
+  set target_port [lindex $all_target_ports $i]
+
   disconnect_net -net $dead_net -objects [get_ports $target_port]
 }
 
-# split into several loops
-# somehow this prevents Vivado from crashing
-foreach anchor $anchors {
-  set dead_net [ get_nets -of_objects [get_cells $anchor] -filter { NAME !~ "*ap_clk*" && NAME !~ "*const*" && ROUTE_STATUS == "HIERPORT" } ]
-  set chosen_net [ get_nets -of_objects [get_cells $anchor] -filter { NAME !~ "*ap_clk*" && NAME !~ "*const*" && ROUTE_STATUS != "HIERPORT" } ]
-  set target_port [get_ports -of_objects [get_nets $dead_net]]
+for {set i 0} {$i < $num} {incr i} {
+  set anchor [lindex $anchors $i]
+  set dead_net [lindex $all_dead_nets $i]
+  set chosen_net [lindex $all_chosen_nets $i]
+  set target_port [lindex $all_target_ports $i]
 
   # disconnect_net -quiet -net $chosen_net -objects [get_pins -of_objects [get_cells $anchor]]
   foreach pin [get_pins -of_objects $chosen_net] {
@@ -28,12 +44,10 @@ foreach anchor $anchors {
   disconnect_net -net $chosen_net -objects [get_pins $dead_pin]
 }
 
-# split into several loops
-# somehow this prevents Vivado from crashing
-foreach anchor $anchors {
-  set dead_net [ get_nets -of_objects [get_cells $anchor] -filter { NAME !~ "*ap_clk*" && NAME !~ "*const*" && ROUTE_STATUS == "HIERPORT" } ]
-  set chosen_net [ get_nets -of_objects [get_cells $anchor] -filter { NAME !~ "*ap_clk*" && NAME !~ "*const*" && ROUTE_STATUS != "HIERPORT" } ]
-  set target_port [get_ports -of_objects [get_nets $dead_net]]
+for {set i 0} {$i < $num} {incr i} {
+  set chosen_net [lindex $all_chosen_nets $i]
+  set target_port [lindex $all_target_ports $i]
+
   connect_net -net $chosen_net -objects [get_ports $target_port]
 }
 
@@ -57,3 +71,6 @@ connect_net -net ap_clk -objects [get_ports ap_clk]
 # unroute the clock net
 set_property IS_ROUTE_FIXED 0 [get_nets ap_clk]
 route_design -unroute -nets [get_nets ap_clk]
+
+# remove dead nets
+opt_design
