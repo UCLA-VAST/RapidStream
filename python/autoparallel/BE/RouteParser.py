6eedba2c3accf37e378c8f4bcafa578ac682cf70
Licheng-Guo <lichengguo777@gmail.com>
2021-07-01 20:37:42 -0700

    [clock] fix format bug

--- python/autoparallel/BE/RouteParser.py
+++ python/autoparallel/BE/RouteParser.py
@@ -21,11 +21,11 @@ class Node:
 
     # find children from the rest of the nodes
     while remaining_tokens:
-      closure, remaining_tokens = self.getClosure(self, remaining_tokens)
+      closure, remaining_tokens = self.getClosure(remaining_tokens)
       self.children.append(Node(closure))
 
 
-  def getClosure(tokens: List):
+  def getClosure(self, tokens: List):
     """
     if the first token is '{', return tokens until associated '}'
     else, return the first token
@@ -60,10 +60,24 @@ class Node:
       return self.name
     else:
       route = self.name + ' '
-      for i in range(self.children)-1:
-        route += '{ ' + self.children[i].dumpRouteString() + '} '
-      route += self.children[-1].dumpRouteString() + ' '
+      for i in range(len(self.children)-1):
+        route += '{ ' + self.children[i].dumpRouteString() + ' } '
+      route += self.children[-1].dumpRouteString()
       
       return route
 
-  
+def compareRouteString(str1, str2):
+  token_list_1 = [t for t in str1.split() if t]
+  token_list_2 = [t for t in str2.split() if t]
+  len1 = len(token_list_1)
+  len2 = len(token_list_2)
+  for i in range(min(len1, len2)):
+    print(token_list_1[i] == token_list_2[i], token_list_1[i], token_list_2[i])
+
+if __name__ == '__main__':
+  test_input = '{ CLK_BUFGCE_9_CLK_OUT CLK_CMT_MUX_16_ENC_2_CLK_OUT CLK_CMT_MUX_2TO1_19_CLK_OUT CLK_HROUTE_0_2 CLK_HROUTE_L2 CLK_HROUTE_L2 CLK_CMT_MUX_3TO1_2_CLK_OUT CLK_VROUTE_BOT CLK_CMT_DRVR_TRI_ESD_3_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_2_CLK_OUT CLK_VROUTE_BOT CLK_CMT_DRVR_TRI_ESD_2_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP { CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B { CLK_CMT_MUX_3TO1_1_CLK_OUT CLK_VDISTR_TOP CLK_CMT_DRVR_TRI_ESD_0_CLK_OUT_SCHMITT_B CLK_BUFCE_ROW_FSR_0_CLK_IN CLK_BUFCE_ROW_FSR_0_CLK_OUT CLK_TEST_BUF_SITE_1_CLK_IN } CLK_BUFCE_ROW_FSR_0_CLK_IN CLK_BUFCE_ROW_FSR_0_CLK_OUT CLK_TEST_BUF_SITE_1_CLK_IN } CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_CMT_MUX_3TO1_0_CLK_OUT CLK_VDISTR_BOT CLK_CMT_DRVR_TRI_ESD_1_CLK_OUT_SCHMITT_B CLK_BUFCE_ROW_FSR_0_CLK_IN CLK_BUFCE_ROW_FSR_0_CLK_OUT CLK_TEST_BUF_SITE_1_CLK_IN }'
+  tokens = [t for t in test_input.split() if t]
+  tree = Node(tokens)
+
+  rebuild_tree = '{ ' + tree.dumpRouteString() + ' }'
+  print(test_input == rebuild_tree)
