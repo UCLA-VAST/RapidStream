218edd0be469afd5f3633afa85eb6bb9165a61a3
Licheng-Guo <lichengguo777@gmail.com>
2021-07-25 14:21:36 -0700

    [flow] update main script

--- examples/mm/16x12/run_back_end.sh
+++ examples/mm/16x12/run_back_end.sh
@@ -9,18 +9,25 @@ export GRB_LICENSE_FILE=/home/einsx7/gurobi.lic
 SERVER_LIST=("u5" "u15" "u17" "u18")
 
 VIV_VER="2020.1"
+ROUTE_VIV_VER="2020.2"
 
 # test not using the RW-unlocked dcps
-USE_UNIQUE_SYNTH_DCP=0
+USE_UNIQUE_SYNTH_DCP=1
+UNIQUE_SLOT_SYNTH_PATH="/expr/mm/16x12_test_07_24/unique_slot_synth"
 
 mkdir $BASE_DIR
 
-python3.6 -m autoparallel.BE.SlotSynthesis ${HUB} ${BASE_DIR} ${VIV_VER}
+if [[ ${USE_UNIQUE_SYNTH_DCP} -eq 1 ]]; then
+    cp -r ${UNIQUE_SLOT_SYNTH_PATH} $BASE_DIR/
+else
+    python3.6 -m autoparallel.BE.SlotSynthesis ${HUB} ${BASE_DIR} ${VIV_VER}
+fi
+
 python3.6 -m autoparallel.BE.InitialSlotPlacement ${HUB} ${BASE_DIR} ${VIV_VER} ${USE_UNIQUE_SYNTH_DCP}
 python3.6 -m autoparallel.BE.PairwiseAnchorPlacement $HUB $BASE_DIR SETUP 0
 python3.6 -m autoparallel.BE.OptSlotPlacement ${HUB} ${BASE_DIR} ${VIV_VER}
 python3.6 -m autoparallel.BE.Clock.SlotAnchorClockRouting  ${HUB} ${BASE_DIR} ${VIV_VER}
-python3.6 -m autoparallel.BE.SlotRouting ${HUB} ${BASE_DIR} "2020.2"
+python3.6 -m autoparallel.BE.SlotRouting ${HUB} ${BASE_DIR} ${ROUTE_VIV_VER}
 python3.6 -m autoparallel.BE._TestPairwiseRouteStitching ${HUB} ${BASE_DIR} ${VIV_VER}
 
 for server in ${SERVER_LIST[*]} ; do
@@ -30,9 +37,11 @@ done
 wait
 
 # synthesis
-for server in ${SERVER_LIST[*]} ; do
-    ssh ${server} "cd ${BASE_DIR}/slot_synth/ && parallel < parallel-synth-${server}.txt" &
-done
+if [[ ${USE_UNIQUE_SYNTH_DCP} -eq 1 ]]; then
+    for server in ${SERVER_LIST[*]} ; do
+        ssh ${server} "cd ${BASE_DIR}/slot_synth/ && parallel < parallel-synth-${server}.txt" &
+    done
+fi
 
 # placement
 for server in ${SERVER_LIST[*]} ; do
@@ -40,7 +49,6 @@ for server in ${SERVER_LIST[*]} ; do
 done
 
 # fire all ILP anchor placement in one server
-# need to source Gurobi set up script
 for server in ${SERVER_LIST[*]} ; do
     ssh ${server} "source /home/einsx7/.bashrc && cd ${BASE_DIR}/ILP_anchor_placement_iter0/ && parallel < parallel-ilp-placement-iter0-${server}.txt" &
 done
