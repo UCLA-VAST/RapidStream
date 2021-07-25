5810ed10924c69c63b762bf1ce1e71a84e23952e
Licheng-Guo <lichengguo777@gmail.com>
2021-07-25 10:02:10 -0700

    [example] distributed backend flow

--- examples/mm/16x12/run_back_end.sh
+++ examples/mm/16x12/run_back_end.sh
@@ -1,47 +1,61 @@
 export HUB=${PWD}/front_end_result.json
-export BASE_DIR=/expr/mm/16x12_fix_anchor_clock_no_double_reg
+export BASE_DIR=/expr/mm/16x12_test_07_25/
 
-# set up Gurobi solver
 export GUROBI_HOME="/home/einsx7/pr/solver/gurobi902/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE=/home/einsx7/gurobi.lic
 
-# prepare the scripts up to slot opt
-python3.6 -m autoparallel.BE.BEManager ${HUB} ${BASE_DIR}
-python3.6 -m autoparallel.BE.OptSlotPlacement ${HUB} ${BASE_DIR}
+SERVER_LIST=("u5" "u15" "u17" "u18")
+
+VIV_VER="2020.1"
+
+# test not using the RW-unlocked dcps
+USE_UNIQUE_SYNTH_DCP=0
+
+mkdir $BASE_DIR
+
+python3.6 -m autoparallel.BE.SlotSynthesis ${HUB} ${BASE_DIR} ${VIV_VER}
+python3.6 -m autoparallel.BE.InitialSlotPlacement ${HUB} ${BASE_DIR} ${VIV_VER} ${USE_UNIQUE_SYNTH_DCP}
 python3.6 -m autoparallel.BE.PairwiseAnchorPlacement $HUB $BASE_DIR SETUP 0
-python3.6 -m autoparallel.BE.SlotRouting ${HUB} ${BASE_DIR}
-python3.6 -m autoparallel.BE._TestPairwiseRouteStitching ${HUB} ${BASE_DIR}
+python3.6 -m autoparallel.BE.OptSlotPlacement ${HUB} ${BASE_DIR} ${VIV_VER}
+python3.6 -m autoparallel.BE.Clock.SlotAnchorClockRouting  ${HUB} ${BASE_DIR} ${VIV_VER}
+python3.6 -m autoparallel.BE.SlotRouting ${HUB} ${BASE_DIR} "2020.2"
+python3.6 -m autoparallel.BE._TestPairwiseRouteStitching ${HUB} ${BASE_DIR} ${VIV_VER}
 
-# fire all slot synthesis & placement. In this example, run everything in one server.
-# beware running out of memory
-
-# rsync -azh --delete -r ${BASE_DIR}/ einsx7@u17:${BASE_DIR}
-# rsync -azh --delete -r ${BASE_DIR}/ einsx7@u18:${BASE_DIR}
-# rsync -azh --delete -r ${BASE_DIR}/ einsx7@u15:${BASE_DIR}
-# ssh u15 "cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u15.txt" &
-# ssh u17 "cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u17.txt" &
-# ssh u18 "cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u18.txt" &
-cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u5.txt &
-cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u15.txt &
-cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u17.txt &
-cd ${BASE_DIR}/parallel_run/ && parallel < parallel-place-all-u18.txt &
-
-# fire all ILP anchor placement in one server
-cd ${BASE_DIR}/ILP_anchor_placement_iter0 && parallel < parallel-ilp-placement-iter0.txt &
-cd ${BASE_DIR}/opt_placement_iter0 && parallel < parallel-opt-placement-u5.txt &
+for server in ${SERVER_LIST[*]} ; do
+    rsync -azh --delete -r ${BASE_DIR}/ einsx7@${server}:${BASE_DIR} &
+done
 
 wait
 
-# get the clock route to all anchors surrounding a slot
-python3.6 -m autoparallel.BE.Clock.SlotAnchorClockRouting ${HUB} ${BASE_DIR}
+# synthesis
+for server in ${SERVER_LIST[*]} ; do
+    ssh ${server} "cd ${BASE_DIR}/slot_synth/ && parallel < parallel-synth-${server}.txt" &
+done
+
+# placement
+for server in ${SERVER_LIST[*]} ; do
+    ssh ${server} "cd ${BASE_DIR}/init_slot_placement/ && parallel < parallel-place-all-${server}.txt" &
+done
+
+# fire all ILP anchor placement in one server
+# need to source Gurobi set up script
+for server in ${SERVER_LIST[*]} ; do
+    ssh ${server} "source /home/einsx7/.bashrc && cd ${BASE_DIR}/ILP_anchor_placement_iter0/ && parallel < parallel-ilp-placement-iter0-${server}.txt" &
+done
+
+for server in ${SERVER_LIST[*]} ; do
+    ssh ${server} "cd ${BASE_DIR}/opt_placement_iter0/ && parallel < parallel-opt-placement-${server}.txt" &
+done
+
+wait
+
 cd ${BASE_DIR}/slot_anchor_clock_routing && parallel < parallel-run-slot-clock-routing.txt
 
-# route slots
-cd ${BASE_DIR}/slot_routing && parallel < parallel-route-with-ooc-clock-u5.txt
+# routing
+for server in ${SERVER_LIST[*]} ; do
+    ssh ${server} "cd ${BASE_DIR}/slot_routing/ && parallel < parallel-route-with-ooc-clock-${server}.txt" &
+done
 
-python3.6 -m autoparallel.BE.SLRLevelStitch ${HUB} ${BASE_DIR}
-
-cd ${BASE_DIR}/SLR_level_stitch && parallel < parallel_slr_stitch.txt
-cd ${BASE_DIR}/top_stitch && VIV_VER=2020.1 vivado -mode gui -source stitch.tcl
+wait
