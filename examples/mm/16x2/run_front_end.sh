b8931c80e16c26d893ea9523651697402cc4ad14
Licheng-Guo <lichengguo777@gmail.com>
2021-07-26 23:07:27 -0700

    [example] remove gurobi and peregrine dependency

--- examples/mm/16x2/run_front_end.sh
+++ examples/mm/16x2/run_front_end.sh
@@ -1,10 +1,7 @@
 # source /curr/einsx7/.bashrc
-export GUROBI_HOME="/home/einsx7/pr/solver/gurobi902/linux64"
-export PATH="${PATH}:${GUROBI_HOME}/bin"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-export GRB_LICENSE_FILE=/home/einsx7/gurobi.lic
-
-cd /home/einsx7/auto-parallel/Peregrine/ && source setup.sh
-cd -
+# export GUROBI_HOME="/home/einsx7/pr/solver/gurobi902/linux64"
+# export PATH="${PATH}:${GUROBI_HOME}/bin"
+# export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+# export GRB_LICENSE_FILE=/home/einsx7/gurobi.lic
 
 python3.6 -m autoparallel.FE.Manager config.json
