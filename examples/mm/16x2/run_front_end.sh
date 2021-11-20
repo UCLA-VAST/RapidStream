fe5f47b076a808b97669e75ef5c5cc8b22fe4a85
Licheng-Guo <lichengguo777@gmail.com>
2021-11-20 15:35:23 -0800

    [example] mm

--- examples/mm/16x2/run_front_end.sh
+++
@@ -1,7 +0,0 @@
-# source /curr/einsx7/.bashrc
-# export GUROBI_HOME="/home/einsx7/pr/solver/gurobi902/linux64"
-# export PATH="${PATH}:${GUROBI_HOME}/bin"
-# export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-# export GRB_LICENSE_FILE=/home/einsx7/gurobi.lic
-
-python3.6 -m autoparallel.FE.Manager config.json
---
+++ examples/mm/run.sh
@@ -0,0 +1,27 @@
+RAPID_STREAM_PATH=/home/einsx7/auto-parallel/src
+
+source ${RAPID_STREAM_PATH}/bash/setup.sh
+
+export HLS_PROJECT_PATH="./16x12/mm_16x12"
+export SERVER_LIST=("u5" "u15" "u17" "u18")
+export USER_NAME=einsx7
+export RUN_DIR=/expr/mm/16x12_3FF_double_reg_rerun_11_20
+
+################################################
+
+echo "[preparing] Unzip the pre-syntehsized HLS project..."
+unzip -q mm_16x12.zip
+
+. ${RAPID_STREAM_PATH}/bash/prepare.sh
+
+echo "[front end] Running the front end (phase 1)..."
+python3.6 -m autoparallel.FE.Manager mm_16x12_config.json
+
+echo "[back end] Running the back end (phase 2 and 3)..."
+${RAPID_STREAM_PATH}/bash/run_back_end.sh \
+    --run-dir ${RUN_DIR} \
+    --front-end-result ./front_end_result.json \
+    --vivado-ver 2021.1 \
+    --target-period 2.7 \
+    --server-list "${SERVER_LIST[*]}" \
+    --user-name ${USER_NAME}
