1e58ae587ef33b076b2113e8696ccc162b2b8b6c
Licheng-Guo <lichengguo777@gmail.com>
2021-12-05 15:19:17 -0800

    [flow] skip the Y/n in apt-get

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # exit when any command fails
-set -e
+set -e +x
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 RAPID_STREAM_PATH=${SCRIPT_DIR}
 
-sudo apt install git
-sudo apt install python3-pip
+sudo apt install -y git
+sudo apt install -y python3-pip
 
 # rapidstream
 python3 -m pip install --editable ${RAPID_STREAM_PATH}/python
@@ -30,14 +30,14 @@ mkdir ${RAPID_STREAM_PATH}/gurobi
 wget https://packages.gurobi.com/9.5/gurobi9.5.0_linux64.tar.gz -P ${RAPID_STREAM_PATH}/gurobi
 tar -xvf ${RAPID_STREAM_PATH}/gurobi/* -C ${RAPID_STREAM_PATH}/gurobi/
 
-sudo apt install rsync
-sudo apt install parallel
-sudo apt install iverilog
-sudo apt install default-jre
-sudo apt install default-jdk
+sudo apt install -y rsync
+sudo apt install -y parallel
+sudo apt install -y iverilog
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
 python3 -m pip install psutil
 
-echo "Finished installation"
-echo "Please update RAPID_STREAM_PATH in rapidstream_setup.sh"
-echo "Please Obtain a Gurobi license at https://www.gurobi.com/downloads/end-user-license-agreement-academic/"
-echo "Then update GRB_LICENSE_FILE in rapidstream_setup.sh to point to your Gurobi license file"
+echo "\nFinished RapidStream installation"
+echo "\nPlease update RAPID_STREAM_PATH in rapidstream_setup.sh"
+echo "\nPlease Obtain a Gurobi license at https://www.gurobi.com/downloads/end-user-license-agreement-academic/"
+echo "\nThen update GRB_LICENSE_FILE in rapidstream_setup.sh to point to your Gurobi license file"
