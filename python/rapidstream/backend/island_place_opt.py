a9945cbe83b5f82831df384d33a477b2b1faafe9
Licheng-Guo <lichengguo777@gmail.com>
2022-09-20 15:09:15 -0700

    opt(place): add option to re-place the island

--- python/rapidstream/backend/island_place_opt.py
+++ python/rapidstream/backend/island_place_opt.py
@@ -2,9 +2,11 @@ import click
 import json
 import os
 import shutil
+from collections import defaultdict
 from typing import Dict, List
 
 from .util import ParallelManager, get_local_anchor_list
+from .floorplan_const import get_neighbor_slice_site_of_laguna_site
 
 @click.command()
 @click.option(
@@ -50,6 +52,7 @@ def setup_island_placement_opt_inner(
     anchor_place_dir: str,
     place_opt_dir: str,
     top_name: str,
+    rerun_placement: bool,
 ):
   """"""
 
@@ -69,7 +72,7 @@ def setup_island_placement_opt_inner(
   for slot_name in config['vertices'].keys():
     os.mkdir(f'{place_opt_dir}/{slot_name}')
     script = get_island_opt_script(
-      config, top_name, slot_name, anchor_to_loc, init_place_dir, place_opt_dir
+      config, top_name, slot_name, anchor_to_loc, init_place_dir, place_opt_dir, rerun_placement,
     )
     open(f'{place_opt_dir}/{slot_name}/island_phys_opt.tcl', 'w').write('\n'.join(script))
     mng.add_task(f'{place_opt_dir}/{slot_name}/', 'island_phys_opt.tcl')
@@ -101,6 +104,25 @@ def get_local_anchor_to_loc(
   return local_anchor_to_loc
 
 
+def map_laguna_anchors_to_slice(anchor_to_loc: Dict[str, str]) -> Dict[str, str]:
+  """A laguna site can hold up to 24 sll connections, but the nearby SLICE that
+    share the same INT can only hold up to 16 FFs. Thus if the right side SLICE is full,
+    we map to the SLICE on the left side
+  """
+  anchor_to_slice_loc = {}
+  slice_counter = defaultdict(int)
+  for anchor, loc in anchor_to_loc.items():
+    if loc.startswith('LAGUNA'):
+      loc_right, loc_left = get_neighbor_slice_site_of_laguna_site(loc)
+      if slice_counter[loc_right] < 16:
+        loc = loc_right
+      else:
+        loc = loc_left
+      slice_counter[loc] += 1
+    anchor_to_slice_loc[anchor] = loc
+  return anchor_to_slice_loc
+
+
 def get_island_opt_script(
   config: Dict,
   top_name: str,
@@ -108,6 +130,7 @@ def get_island_opt_script(
   anchor_to_loc: Dict[str, str],
   init_place_dir: str,
   place_opt_dir: str,
+  rerun_placement: bool,
 ) -> List[str]:
   script = []
 
@@ -118,11 +141,17 @@ def get_island_opt_script(
 
   script.append(f'open_checkpoint {init_place_dir}/{slot_name}/{slot_name}_place.dcp')
 
-  # remove the location of anchors from last iteration
-  script.append('unplace_cell { \\')
-  for anchor in local_anchor_to_loc.keys():
-    script.append(f'  {anchor} \\')
-  script.append('}')
+  if rerun_placement:
+    # map laguna anchors to nearby slice sites
+    # avoid connectitivy DRC in place_design
+    local_anchor_to_loc = map_laguna_anchors_to_slice(local_anchor_to_loc)
+    script.append('place_design -unplace')
+  else:
+    # remove the location of anchors from last iteration
+    script.append('unplace_cell { \\')
+    for anchor in local_anchor_to_loc.keys():
+      script.append(f'  {anchor} \\')
+    script.append('}')
 
   # dictate the location for each anchor
   script.append('place_cell { \\')
@@ -130,7 +159,10 @@ def get_island_opt_script(
     script.append(f'  {anchor} {loc} \\')
   script.append('}')
 
-  script.append('phys_opt_design')
+  if rerun_placement:
+    script.append('place_design')
+  else:
+    script.append('phys_opt_design')
 
   # extract locations of src and dst of each anchor
   script.append(f'set kernel_cell_addr "{kernel_cell_addr}"')
